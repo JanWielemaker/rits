02f7244a74674f899c01992ede6ca7e01dd0f49c
Markus Triska <markus.triska@gmx.at>
2015-01-11 11:46:37 +0100

    fixes

--- rits_engine.pl
+++ rits_engine.pl
@@ -90,14 +90,14 @@ help_for_wrong_answer(cancel(A/B), _, Hist) -->
 % Help for common multiple
 
 help_for_wrong_answer(cm(X,Y), _, Hist) -->
-        { list_internals(Hist, [cm(X,Y),cm(X,Y)|_],
+        { list_internals(Hist, [cm(X,Y),cm(X,Y)|_]) },
         [format("I see you are having a hard time with this.\n")],
         { CM is X*Y },
-        [format("    Hint: ~w (~w * ~w) is a possible solution.\n", [CM,X,Y])].
-help_for_wrong_answer(cm(X,Y), A, _) :-
+        [format("Hint: ~w (~w * ~w) is a possible solution.\n", [CM,X,Y])].
+help_for_wrong_answer(cm(X,Y), A, _) -->
         { A mod X =\= 0 },
         [format("~w is not a common multiple of ~w and ~w, since ~w is not divisible by ~w!\n", [A,X,Y,A,X])].
-help_for_wrong_answer(cm(X,Y), A, _) :-
+help_for_wrong_answer(cm(X,Y), A, _) -->
         { A mod Y =\= 0 },
         [format("    ~w is no common multiple of ~w and ~w, since ~w is not divisible by ~w!\n", [A,X,Y,A,Y])].
 
