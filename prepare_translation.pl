bca983191f65c70ca17d97d175374ed35bcc51b4
Markus Triska <markus.triska@gmx.at>
2015-01-11 23:09:14 +0100

    more

--- prepare_translation.pl
+++ prepare_translation.pl
@@ -2,24 +2,47 @@
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    Prepare translation templates.
 
-   Usage: $ swipl -q -f prepare_translation.pl < rits_engine.pl
+   One template is created for each string that can possibly be
+   "emitted" by RITS.
+
+   In this example, we are preparing Portuguese translations.
+
+   Usage:
+
+       $ swipl -q -f prepare_translation.pl < rits_engine.pl
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 run :-
         repeat,
             read(Term),
             (   Term == end_of_file -> halt
-            ;   Term = (A --> B),
-                (   process_grammar_body(B) -> true
-                ;   error(could_not_process(B))
+            ;   Term = (_ --> Body),
+                (   process_grammar_body(Body) -> true
+                ;   writeln(could_not_process(Body)),
+                    halt
                 )
             ;   true
             ),
             false.
 
-process_grammar_body({_Goal}).
-process_grammar_body([L|Ls]) :- process_list([L|Ls]).
-process_grammar_body(( Cond -> Then ; Else)) :-
-        process_grammar_body(Cond),
+process_grammar_body([]).
+process_grammar_body(!).
+process_grammar_body({_}) :- !.
+process_grammar_body((G1,G2)) :- !,
+        process_grammar_body(G1),
+        process_grammar_body(G2).
+process_grammar_body([L|Ls]) :- !, maplist(process_token, [L|Ls]).
+process_grammar_body(( _ -> Then ; Else)) :- !,
         process_grammar_body(Then),
         process_grammar_body(Else).
+% for information, see what is being ignored
+%process_grammar_body(I) :- !, format("%% ignoring: ~w\n", [I]).
+process_grammar_body(I).
+
+process_token(format(Xs, _)) :- !,
+        format("translation(~q, pt,\n~t~12+\"\").\n", [Xs]).
+process_token(format(Xs))    :- !,
+        format("translation(~q, pt,\n~t~12+\"\").\n", [Xs]).
+process_token(_). % ignore
+
+:- initialization(run).
