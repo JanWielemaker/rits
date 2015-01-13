0f21c3d596e2f1a2fc321e963efa645a981b91e4
Markus Triska <markus.triska@gmx.at>
2015-01-13 23:38:29 +0100

    read_answer is already taken care of by the caller

--- rits_multiple_choice.pl
+++ rits_multiple_choice.pl
@@ -1,12 +1,9 @@
 :- module(rits_multiple_choice, []).
 
-rits:solve(mchoice(Text,Options,_)) -->
-        [translation(Text),
-         choices(Options),
-         read_answer].
+rits:solve(mchoice(Text,Options,_)) --> [translation(Text),choices(Options)].
 
 rits:actions(mchoice(Text,Options,Solution), Answer, _) -->
-        (   { sort(Answer, Ls), sort(Solution, Ls) } ->
+        (   { Solution == Answer } ->
             [format("Correct. Very nice!\n\n")]
         ;   [format("Wrong.\n")],
             [solve(mchoice(Text,Options,Solution))]
