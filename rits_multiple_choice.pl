9b0246865539dda24168f2b0b663a8e8856e1244
Markus Triska <markus.triska@gmx.at>
2015-01-13 22:35:33 +0100

    module for multiple choice tests

---
+++ rits_multiple_choice.pl
@@ -0,0 +1,12 @@
+:- module(rits_multiple_choice, []).
+
+rits:solve(mchoice(Text,Options,Solution)) -->
+        [translation(Text),
+         choices(Options)].
+
+rits:actions(mchoice(Text,Options,Solution)) -->
+        (   { sort(Options, Ls), sort(Solution, Ls) } ->
+            [format("Correct. Very nice!\n\n")]
+        ;   [format("Wrong.\n")],
+            [solve(mchoice(Text,Options,Solution))]
+        ).
