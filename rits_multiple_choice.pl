fd1a324c6106b04cd33567edf8ff6fcbe28eee79
Markus Triska <markus.triska@gmx.at>
2015-01-28 23:09:35 +0100

    shorten

--- rits_multiple_choice.pl
+++ rits_multiple_choice.pl
@@ -4,14 +4,12 @@
 
 rits:solve(mchoice(Text,Options,_)) --> [translation(Text),choices(Options)].
 
-
-rits:actions(mchoice(_,_,Solution), Answer, _) -->
-        { Solution == Answer },
-        "Correct. Very nice!\n\n".
 rits:actions(mchoice(_,_,_), Answer, _) -->
         { \+ integer(Answer) },
         "The answer must be the integer corresponding to the solution.\n",
         again.
+rits:actions(mchoice(_,_,Solution), Solution, _) -->
+        "Correct. Very nice!\n\n".
 rits:actions(mchoice(_,_,_), _, _) -->
         "Wrong.\n",
         again.
