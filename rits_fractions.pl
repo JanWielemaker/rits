377a5ebe3bb328f1f2834c97c91d770ab64f6191
Markus Triska <markus.triska@gmx.at>
2015-01-28 19:05:13 +0100

    use plain character codes instead of format/1

--- rits_fractions.pl
+++ rits_fractions.pl
@@ -9,23 +9,23 @@ to_rational(A0+B0, A + B)  :- !, to_rational(A0, A), to_rational(B0, B).
 to_rational(A/B, A rdiv B) :- !.
 
 rits:solve(A/B + C/D) -->
-        [format("Please solve:\n\n~t~10+"),
-         fraction_layout(A/B + C/D)].
+        "Please solve:\n\n~t~10+",
+        [fraction_layout(A/B + C/D)].
 rits:solve(cancel(X/Y)) -->
-        [format("Please cancel common divisors in:\n\n~t~10+"),
-         fraction_layout(X/Y)].
+        "Please cancel common divisors in:\n\n~t~10+",
+        [fraction_layout(X/Y)].
 
 
 rits:actions(cancel(A/B), Answer0, Hist) -->
         (   { Answer0 = X / Y } ->
             (   { Y = 0 } ->
-                [format("The denominator of a fraction cannot be 0.\n")],
+                "The denominator of a fraction cannot be 0.\n",
                  again
             ;   { A rdiv B =:= X rdiv Y } ->
-                [format("Good, the solution is correct")],
+                "Good, the solution is correct",
                 (   { gcd(X,Y) =:= 1 } ->
-                    [format(" and also minimal. Very nice!\n\n")]
-                ;   [format(", but not minimal.\n"), solve(cancel(X/Y))]
+                    " and also minimal. Very nice!\n\n"
+                ;   ", but not minimal.\n", [solve(cancel(X/Y))]
                 )
             ;   wrong,
                 help_for_wrong_answer(cancel(A/B), Answer0, Hist),
@@ -33,23 +33,23 @@ rits:actions(cancel(A/B), Answer0, Hist) -->
             )
         ;   { integer(Answer0) } ->
             (   { A mod B =:= 0, Answer0 =:= A//B } ->
-                [format("Good, the solution is correct and also minimal. Very nice!\n\n")]
+                "Good, the solution is correct and also minimal. Very nice!\n\n"
             ;   wrong,
                 help_for_wrong_answer(cancel(A/B), Answer0, Hist),
                 again
             )
-        ;   [format("The answer must be an integer or a fraction.\n")],
+        ;   "The answer must be an integer or a fraction.\n",
              again
         ).
 rits:actions(Expression0, Answer0, Hist) -->
         { to_rational(Expression0, Expression),
           to_rational(Answer0, Answer) },
         (   { catch(Expression =:= Answer,_,false) } ->
-            [format("Good, the solution is correct")],
+            "Good, the solution is correct",
             { Shorter is Answer },
             (   { Shorter = Answer } ->
-                [format(" and also minimal. Very nice!\n\n")]
-            ;   [format(", but not minimal.\n"), solve(cancel(Answer0))]
+                " and also minimal. Very nice!\n\n"
+            ;   ", but not minimal.\n", [solve(cancel(Answer0))]
             )
         ;   wrong,
             help_for_wrong_answer(Expression0, Answer0, Hist),
@@ -59,34 +59,34 @@ rits:actions(Expression0, Answer0, Hist) -->
 
 help_for_wrong_answer(cancel(A/B), _, Hist) -->
         { Hist = [cancel(A/B)=_,cancel(A/B)=_,cancel(A/B)=_|_] },
-        [format("I see you are having a hard time with this.\n"),
-         format("Hint: Find a common divisor of ~w and ~w.\n", [A,B])].
+        "I see you are having a hard time with this.\n",
+        [format("Hint: Find a common divisor of ~w and ~w.\n", [A,B])].
 help_for_wrong_answer(_/B + _/D, _/Y, _) -->
         { least_common_multiple(B, D, Y) },
-        [format("The denominator is suitable, but the numerator is wrong!\n")].
+        "The denominator is suitable, but the numerator is wrong!\n".
 help_for_wrong_answer(_/B + _/D, _/Y, _) -->
         { Y mod B =:= 0,
           Y mod D =:= 0 },
-        [format("The denominator is suitable, but the numerator is wrong!\n"),
-         format("Use a smaller common multiple as denominator to make this easier.\n")].
+        "The denominator is suitable, but the numerator is wrong!\n",
+        "Use a smaller common multiple as denominator to make this easier.\n".
 help_for_wrong_answer(A/B + C/D, X / _, Hist) -->
         { B =\= D,
           X =:= A + C },
-        [format("You cannot just sum the numerators when the denominators are different!\n\n")],
+        "You cannot just sum the numerators when the denominators are different!\n\n",
         (   { member(cm(B,D)=Answer, Hist), least_common_multiple(B,D,Answer) } ->
             [format("Recall that you have already found the least common multiple of ~w and ~w!\n", [B,D]),
              format("First rewrite the fractions so that the denominator is ~w for both, then add.\n", [Answer])]
         ;   { member(cm(B,D)=Answer, Hist), Answer =\= 0, Answer mod B =:= 0, Answer mod D =:= 0 } ->
             [format("Recall that you have already found a common multiple of ~w and ~w: ~w\n", [B,D,Answer]),
-             format("You can either use that, or find a smaller multiple to make it easier.\n")]
+             "You can either use that, or find a smaller multiple to make it easier.\n"]
         ;   [subproblem([format("Let us first find a common multiple of ~w and ~w!\n", [B,D]),
                          solve(cm(B,D)),
-                         format("Now apply this knowledge to the original task!\n")])]
+                         "Now apply this knowledge to the original task!\n"])]
         ).
 help_for_wrong_answer(A/B + C/D, Answer0, _) -->
         { to_rational(Answer0, Answer),
           Answer =:= (A + C) rdiv (B + D) },
-        [format("You should not sum the denominators, but only the numerators!\n")].
+        "You should not sum the denominators, but only the numerators!\n".
 help_for_wrong_answer(_/B + _/_, _ / Y, _) -->
         { Y mod B =\= 0 },
         [format("~w cannot be a common denominator, because it cannot be divided by ~w.\n", [Y,B])].
@@ -97,7 +97,7 @@ help_for_wrong_answer(_/_ + _/D, _ / Y, _) -->
 % Fallback
 
 help_for_wrong_answer(_, _, _) -->
-        [format("Unfortunately, I cannot give any useful hints here.\n")].
+        "Unfortunately, I cannot give any useful hints here.\n".
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    Test cases.
