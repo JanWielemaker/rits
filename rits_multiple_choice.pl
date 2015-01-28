180377fe12168e0122895cff67b2a0d1fe359e9e
Markus Triska <markus.triska@gmx.at>
2015-01-28 23:08:47 +0100

    use again//0 LORITS element

--- rits_multiple_choice.pl
+++ rits_multiple_choice.pl
@@ -8,10 +8,10 @@ rits:solve(mchoice(Text,Options,_)) --> [translation(Text),choices(Options)].
 rits:actions(mchoice(_,_,Solution), Answer, _) -->
         { Solution == Answer },
         "Correct. Very nice!\n\n".
-rits:actions(mchoice(Text,Options,Solution), Answer, _) -->
+rits:actions(mchoice(_,_,_), Answer, _) -->
         { \+ integer(Answer) },
         "The answer must be the integer corresponding to the solution.\n",
-        solve(mchoice(Text,Options,Solution)).
-rits:actions(mchoice(Text,Options,Solution), _, _) -->
+        again.
+rits:actions(mchoice(_,_,_), _, _) -->
         "Wrong.\n",
-        solve(mchoice(Text,Options,Solution)).
+        again.
