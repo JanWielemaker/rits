9e7ca8d380b25c901d2750f7048652322f6e1117
Markus Triska <markus.triska@gmx.at>
2015-01-28 22:05:23 +0100

    ADDED: abbreviation for helping in LORITS

--- lorits.pl
+++ lorits.pl
@@ -5,6 +5,7 @@
 :- module(lorits, [
                    again//0,
                    format//2,
+                   help//1,
                    solve//1,
                    subproblem//1,
                    wrong//0
@@ -19,3 +20,5 @@ format(Str, Args) --> [format(Str, Args)].
 solve(Task) --> [solve(Task)].
 
 subproblem(P) --> [subproblem(P)].
+
+help(Pred) --> wrong, [help(Pred)], again.
