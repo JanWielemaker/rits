03d1434a8a73c61c1e44aefb741cfa98f15f6456
Markus Triska <markus.triska@gmx.at>
2015-01-28 19:14:58 +0100

    use format/2 predefined LORITS action

--- rits_common_multiple.pl
+++ rits_common_multiple.pl
@@ -4,7 +4,7 @@
 :- use_module(lorits).
 
 rits:solve(cm(X,Y)) -->
-        [format("Please enter a common multiple of ~w and ~w:\n\n", [X,Y])].
+        format("Please enter a common multiple of ~w and ~w:\n\n", [X,Y]).
 
 rits:actions(cm(_,_), Answer, _) -->
         { \+ integer(Answer) },
@@ -34,13 +34,13 @@ help_for_wrong_answer(cm(X,Y), _, Hist) -->
         { Hist = [cm(X,Y)=_,cm(X,Y)=_,cm(X,Y)=_|_] },
         "I see you are having a hard time with this.\n",
         { CM is X*Y },
-        [format("Hint: ~w * ~w = ~w is a possible solution.\n", [X,Y,CM])].
+        format("Hint: ~w * ~w = ~w is a possible solution.\n", [X,Y,CM]).
 help_for_wrong_answer(cm(X,Y), A, _) -->
         { A mod X =\= 0 },
-        [format("~w is not a common multiple of ~w and ~w, since ~w is not divisible by ~w!\n", [A,X,Y,A,X])].
+        format("~w is not a common multiple of ~w and ~w, since ~w is not divisible by ~w!\n", [A,X,Y,A,X]).
 help_for_wrong_answer(cm(X,Y), A, _) -->
         { A mod Y =\= 0 },
-        [format("~w is no common multiple of ~w and ~w, since ~w is not divisible by ~w!\n", [A,X,Y,A,Y])].
+        format("~w is no common multiple of ~w and ~w, since ~w is not divisible by ~w!\n", [A,X,Y,A,Y]).
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    Test cases.
