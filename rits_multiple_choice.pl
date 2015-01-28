1162b27c54395488b73add86f7a05005fda775bf
Markus Triska <markus.triska@gmx.at>
2015-01-28 22:09:21 +0100

    use new LORITS elements

--- rits_multiple_choice.pl
+++ rits_multiple_choice.pl
@@ -1,15 +1,17 @@
 :- module(rits_multiple_choice, []).
 
+:- use_module(lorits).
+
 rits:solve(mchoice(Text,Options,_)) --> [translation(Text),choices(Options)].
 
 
 rits:actions(mchoice(_,_,Solution), Answer, _) -->
         { Solution == Answer },
-        [format("Correct. Very nice!\n\n")].
+        "Correct. Very nice!\n\n".
 rits:actions(mchoice(Text,Options,Solution), Answer, _) -->
         { \+ integer(Answer) },
-        [format("The answer must be the integer corresponding to the solution.\n"),
-         solve(mchoice(Text,Options,Solution))].
+        "The answer must be the integer corresponding to the solution.\n",
+        solve(mchoice(Text,Options,Solution)).
 rits:actions(mchoice(Text,Options,Solution), _, _) -->
-        [format("Wrong.\n"),
-         solve(mchoice(Text,Options,Solution))].
+        "Wrong.\n",
+        solve(mchoice(Text,Options,Solution)).
