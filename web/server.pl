c1ce331297b2b705f52aff90dce84d3b7a5c78ab
Markus Triska <markus.triska@gmx.at>
2015-02-01 19:55:30 +0100

    simple HTTP server

---
+++ web/server.pl
@@ -0,0 +1,30 @@
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+   HTTP server.
+
+   Example usage to spawn a server on port 5050:
+
+       $ swipl -f server.pl -g "server(5050)"
+
+   then direct your browser to http://localhost:5050
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+:- module(server,
+	  [ server/1			% +Port
+	  ]).
+
+:- use_module(library(pengines)).
+:- use_module(pengine_sandbox:library(pengines)).
+:- use_module(library(http/thread_httpd)).
+:- use_module(library(http/http_dispatch)).
+:- use_module(library(http/http_server_files)).
+:- use_module(library(http/http_files)).
+
+:- pengine_application(rits).
+:- use_module(rits:library(pengines_io)).
+pengines:prepare_module(Module, rits, _Options) :-
+        pengines_io:pengine_bind_io_to_html(Module).
+
+:- http_handler(/, http_reply_from_files(., []), [prefix]).
+
+server(Port) :-
+	http_server(http_dispatch, [port(Port)]).
