d9eb87673fd4d40cea1107e9e894eb28b879344d
Markus Triska <markus.triska@gmx.at>
2015-02-02 17:53:43 +0100

    Add safe declarations for sandboxing. This should not be necessary!

--- lorits.pl
+++ lorits.pl
@@ -22,3 +22,15 @@ solve(Task) --> [solve(Task)].
 subproblem(P) --> [subproblem(P)].
 
 help(Pred) --> [help(Pred)].
+
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+   TODO: Find a way to structure the modules so that the following
+         declarations are not necessary. These are provably safe.
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+:- multifile
+	sandbox:safe_primitive/1.
+
+sandbox:safe_primitive(lorits:format(_,_,_,_)).
+sandbox:safe_primitive(lorits:again(_,_)).
